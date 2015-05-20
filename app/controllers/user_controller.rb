c2c93a12f9654440b0da969615b5614b5bfe21ea
lawrenceluk <lluk@ucsd.edu>
2015-05-20 01:39:52 -0700

    Starting profile pages

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,21 +1,19 @@
 class UserController < ApplicationController
+	before_action :ensure_user
 
 	def show
-		@user = User.find_by_a_username(params[:id])
 	end
 
 	def update
-		@user = User.find(params[:id])
-		if @user == current_user || current_user.admin
-			@edit = true
-			if request.patch?
-				if @user.update_attributes(user_params)
-					sign_in(@user, :bypass => true)
-					@success = true
-					render 'update'
-				else
-					render 'update'
-				end
+		@edit = true
+		if request.patch?
+			if @user.update_attributes(user_params)
+				sign_in(@user, :bypass => true)
+				flash[:success] = "Profile successfully updated."
+				render 'update'
+			else
+				flash[:alert] = "There may have been errors saving."
+				render 'update'
 			end
 		end
 	end
@@ -27,5 +25,13 @@ class UserController < ApplicationController
       :phone, :family, :line, :pledge_class, :membership_status, :major, :graduation_year, :shirt_size, :car, 
       :password, :password_confirmation)
 	end
+
+	def ensure_user
+		@user = User.find_by_a_username(params[:id])
+		@user ||= User.find(params[:id])
+    unless user_signed_in? && (current_user.id == @user.id || current_user.admin)
+      redirect_to root_path, notice: "You do not have permission to see that page."
+    end
+  end
 
 end
