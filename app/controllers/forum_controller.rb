7588439c92521ba603e68d265e0049e25c525e9a
Matthew Chinn <mattchinn96@gmail.com>
2016-02-07 15:20:54 -0800

    creating, editing, deleting forum/topic/posts all works, next formatting

--- app/controllers/forum_controller.rb
+++ app/controllers/forum_controller.rb
@@ -5,6 +5,29 @@ class ForumController < ApplicationController
     @forums = Forum.all.order(updated_at: :desc)
   end
 
+  def destroy_forum
+    f = Forum.find(params[:id])
+    if f.delete
+      flash[:success] = "Deleted successfully"
+    elsif
+      flash[:alert] = "Delete failed"
+    end
+    redirect_to forum_path
+  end
+
+  def edit_forum
+    @forum = Forum.find(params[:id])
+    if request.patch?
+      if @forum.update_attributes(forum_params)
+        flash[:success] = "Edited successfully"
+      elsif
+        flash[:alert] = "Error saving edit"
+      end
+      redirect_to forum_path
+    end
+  end
+
+
   #create a new forum
   def new
     if request.get?
@@ -40,12 +63,78 @@ class ForumController < ApplicationController
       elsif
         flash[:alert] = "Error creating topic"
       end
-      redirect_to topic_path
+      redirect_to topic_path(@forum)
+    end
+  end
+
+  def destroy_topic
+    t = Topic.find(params[:id])
+    @forum = Forum.find( t.forum_id )
+    if t.delete
+      flash[:success] = "Deleted successfully"
+    elsif
+      flash[:alert] = "Delete failed"
+    end
+    redirect_to topic_path(@forum) 
+  end
+
+  def edit_topic
+    @topic = Topic.find(params[:id])
+    @forum = Forum.find( @topic.forum_id )
+    if request.patch?
+      if @topic.update_attributes(topic_params)
+        flash[:success] = "Edited successfully"
+      elsif
+        flash[:alert] = "Error saving edit"
+      end
+      redirect_to topic_path(@forum) 
+    else
+      render "forum/topic/edit_topic"
     end
   end
 
   def post
+    @topic = Topic.find( params[:id] )
+    if request.post?
+      @post = Post.create
+      if @post.update_attributes(post_params)
+        flash[:success] = "Post created"
+        @topic.update_attribute(:updated_at, @post.updated_at )
+        @forum = Forum.find( @topic.forum_id )
+        @forum.update_attribute(:updated_at, @post.updated_at )
+      else
+        flash[:alert] = "Error creating post"
+      end
+    end
+    @new_post = Post.new
+    @posts = Post.where(topic_id: params[:id]).order(:created_at)
     render "forum/post/show"
+  end
+
+  def edit_post
+    @post = Post.find( params[:id] )
+    @topic = Topic.find(@post.topic_id)
+    if request.patch?
+      if @post.update_attributes(post_params)
+        flash[:success] = "Edited successfully"
+      elsif
+        flash[:alert] = "Error saving edit"
+      end
+      redirect_to post_path(@topic)
+    else
+      render "forum/post/edit_post"
+    end
+  end
+
+  def destroy_post
+    p = Post.find(params[:id])
+    @topic = Topic.find( p.topic_id )
+    if p.delete
+      flash[:success] = "Deleted successfully"
+    elsif
+      flash[:alert] = "Delete failed"
+    end
+    redirect_to post_path(@topic) 
   end
 
   private
@@ -55,6 +144,10 @@ class ForumController < ApplicationController
 
   def topic_params
     params.require(:topic).permit(:title, :creator_id, :forum_id )
+  end
+
+  def post_params 
+    params.require(:post).permit(:comment, :creator_id, :topic_id)
   end
 
   def ensure_signed_in
