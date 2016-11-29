c68c9253cc51931e59204e1e15a4a6ff17df7eac
John <jlsenar@gmail.com>
2016-11-29 20:31:50 +0000

    still fixing conflict

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -17,6 +17,7 @@ class EventController < ApplicationController
 		# 2 drivers: require drive if 14 signups
 		@needs_driver = @event.off_campus &&
 			#attendances.count - (attendances.where(can_drive: true).count * 5) >= 4
+			attendances.count >= (attendances.where(can_drive: true).count * 5)
 		@num_drivers = attendances.where(can_drive: true).count
 		@holiday = matches_holiday(@event.start_time)
 		unless @event.public || current_user.admin
