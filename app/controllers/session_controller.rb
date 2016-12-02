6c1e095a89bd247a103f6238499955380b374175
Brandon <brandonjhuang@gmail.com>
2016-12-02 05:53:53 +0000

    updated session controller to fix deprecated methods

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,6 +4,7 @@ class SessionController < Devise::SessionsController
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_in).push(:login, :username)
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :username])#.for(:sign_in).push(:login, :username)
+                              
   end
 end
