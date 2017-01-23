dc0c87ad6f3cd349cf01ed99753d5918c5b06a0b
Brandon <brandonjhuang@gmail.com>
2017-01-23 06:16:58 +0000

    fixed registration controller, deprecated for method

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -4,6 +4,7 @@ class RegistrationController < Devise::RegistrationsController
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up).push(:username, :firstname, :lastname, :nickname, :phone, :email)
+    #devise_parameter_sanitizer.for(:sign_up).push(:username, :firstname, :lastname, :nickname, :phone, :email)
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :firstname, :lastname, :nickname, :phone, :email])
   end
 end
