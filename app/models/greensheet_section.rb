881408ed326599d2ea544336bdde730e515e13bd
Brandon <brandonjhuang@gmail.com>
2017-03-30 08:14:29 +0000

    I love developing on live servers, next guy don't do this

--- app/models/greensheet_section.rb
+++ app/models/greensheet_section.rb
@@ -2,20 +2,21 @@ class GreensheetSection < ActiveRecord::Base
 belongs_to :user
 
   def self.calculateHours(attendance, event)
-    return 0 if !attendance.attended
-    hours = attendance.drove ? event.driver_hours : event.hours
+    #return 0 if !attendance.attended
+    hours = 0
+    #hours = attendance.drove ? event.driver_hours : event.hours
     unless attendance.flaked || attendance.replacement_flaked
       
       event.driver_hours = event.hours if event.driver_hours.nil?
-      hours = event.driver_hours if attendance.drove
+      hours = attendance.drove ? event.driver_hours : event.hours
       hours = attendance.drove ? (event.driver_hours-event.hours) + 0.5 * event.hours : 0.5*event.hours if attendance.late
     end
 
     if attendance.flaked && event.flake_penalty
-        hours *= -1 
+        hours= event.hours * -1 
     end
     if attendance.replacement_flaked && event.flake_penalty
-        hours *= -0.5 
+        hours = event.hours * -0.5 
     end
 
     return hours
