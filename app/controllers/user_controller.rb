c44a6704e6aedb2b1eb4318c5af566a057be6e28
Brandon <brandonjhuang@gmail.com>
2017-03-30 09:42:52 +0000

    3 strike behavior

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,44 +1,52 @@
 class UserController < ApplicationController
-	before_action :ensure_user
+    before_action :ensure_user
   before_action :set_quarter_cutoff, only: [:show, :greensheet]
 
-	def show
+    def show
     if params[:toggle_admin]
       @user.admin = !@user.admin #toggle admin
       @user.save
     end
-		attendances = Attendance.where(user_id: @user.id)
+        attendances = Attendance.where(user_id: @user.id)
 
-		@attendances = attendances.where("created_at >  ?", @quarter_cutoff )
-		@attended_events = @attendances.map{|x| Event.find(x.event_id)}
+        @attendances = attendances.where("created_at >  ?", @quarter_cutoff )
+        @attended_events = @attendances.map{|x| Event.find(x.event_id)}
     @events = @attended_events.any? ? @attended_events.group_by{|x| x.start_time.strftime("%m/%d (%A)")} : nil
     @events = @events.sort_by { |date, evt| date } if @events #events isnt null
-		# how can I make this prettier
-		@hours = 0
-		@flakehours = 0
-		@fellowships = 0
-		@user.attendances.each do |a|
-			event = Event.find(a.event_id)
-			if event.end_time > @quarter_cutoff
-				if event.event_type == "Service"
-					if a.attended
-						@hours += GreensheetSection.calculateHours(a, event)#a.drove ? event.driver_hours : event.hours
-					elsif a.late
-					  @hours += GreensheetSection.calculateHours(a, event)#a.drove ? (event.driver_hours-event.hours) + 0.5 * event.hours : 0.5*event.hours
-					elsif event.flake_penalty && a.flaked
-						@flakehours += event.hours
-					elsif event.flake_penalty && a.replacement_flaked
-					  @flakehours += 0.5*event.hours
-					end
-				elsif event.event_type == "Fellowship"
-					@fellowships += GreensheetSection.calculateHours(a, event)
-				
-				end
-			end
-		end
-	end
+        # how can I make this prettier
+        @hours = 0
+        @flakehours = 0
+        @fellowships = 0
+        @flake_count = 0
+        @user.attendances.reverse.each do |a|
+            event = Event.find(a.event_id)
+            if event.end_time > @quarter_cutoff
+                if event.event_type == "Service"
+                    if a.attended
+                        @hours += GreensheetSection.calculateHours(a, event)#a.drove ? event.driver_hours : event.hours
+                    elsif a.late
+                        @hours += GreensheetSection.calculateHours(a, event)#a.drove ? (event.driver_hours-event.hours) + 0.5 * event.hours : 0.5*event.hours
+                    elsif event.flake_penalty && a.flaked 
+                        @flake_count = @flake_count + 1
+                        @flakehours += @flake_count*event.hours if @flake_count  < 3
+                        @flakehours += 3*event.hours if @flake_count >=3 
+                    elsif event.flake_penalty && a.replacement_flaked 
+                        @flake_count = @flake_count + 1
+                       @flakehours += 0.5*@flake_count*event.hours if @flake_count  < 3
+                       @flakehours += 1.5*event.hours if @flake_count >=3 
+                    end
+                elsif event.event_type == "Fellowship"
+                    @fellowships += GreensheetSection.calculateHours(a, event)
+                
+                end
+            end
+        end
+        # zero out if three strikes and not super negative
+        @hours = 0 if @hours > @flakehours && flake_count >= 3
+    end
 
-	def greensheet
+    def greensheet
+    @flake_count = 0
     @reqs = Hash.new(0) #contain all the diff requirement counters
     @display = params[:display] #either current or year
 
@@ -84,7 +92,7 @@ class UserController < ApplicationController
 
     if request.get?
       #@user.attendances.where("created_at > ?", @quarter_cutoff).each do |a|
-      @user.attendances.each do |a|
+      @user.attendances.reverse.each do |a|
         event = Event.find(a.event_id)
         
         dont_add = false 
@@ -105,7 +113,11 @@ class UserController < ApplicationController
           p "b"
           #accounts for flaking and all
           hours = GreensheetSection.calculateHours(a, event)
-
+        if event.event_type == "Service" && hours < 0
+             @flake_count = @flake_count + 1
+               hours = @flake_count*hours if @flake_count  < 3
+               hours = 3*hours if @flake_count >=3     
+        end
           if ["Alpha", "Phi", "Omega", "Rho", "Pi"].include? event.event_type
             event.event_type = "Family" #list family for dropdown
           end
@@ -156,56 +168,56 @@ class UserController < ApplicationController
         @reqs[:chalkboarding] += 1 if s.title.include? "Chalkboard"
       end
     end
-
+    @reqs[:hours] = 0 if @reqs[:hours] > 0 && flake_count >= 3
     @sections = @sections.order(:event_type, :start_time )
     @new_section = GreensheetSection.new #for eboard to add new events
-	end
+    end
 
-	def update
-		@edit = true
-		updater = current_user.id
-		if request.patch?
-			user_params[:firstname].strip!
-			user_params[:lastname].strip!
-			if @user.update_attributes(user_params)
-				@user.update_attribute(:displayname, user_params[:firstname] + ' ' + user_params[:lastname])
-				if updater == @user.id
-					sign_in(@user, :bypass => true)
-				end
-				if !request.xhr?
-					flash[:success] = "Profile successfully updated."
-					render 'update'
-				end
-			elsif !request.xhr?
-				flash[:alert] = "There may have been errors saving."
-				render 'update'
-			end
-		end
-	end
+    def update
+        @edit = true
+        updater = current_user.id
+        if request.patch?
+            user_params[:firstname].strip!
+            user_params[:lastname].strip!
+            if @user.update_attributes(user_params)
+                @user.update_attribute(:displayname, user_params[:firstname] + ' ' + user_params[:lastname])
+                if updater == @user.id
+                    sign_in(@user, :bypass => true)
+                end
+                if !request.xhr?
+                    flash[:success] = "Profile successfully updated."
+                    render 'update'
+                end
+            elsif !request.xhr?
+                flash[:alert] = "There may have been errors saving."
+                render 'update'
+            end
+        end
+    end
 
-	def all
-		unless current_user.admin?
-			flash[:alert] = "You do not have permission to access that page."
-			redirect_to root_path
-		end
-		@users = User.order('displayname').where(approved: true)
-	end
+    def all
+        unless current_user.admin?
+            flash[:alert] = "You do not have permission to access that page."
+            redirect_to root_path
+        end
+        @users = User.order('displayname').where(approved: true)
+    end
 
-	private
+    private
 
-	def user_params
+    def user_params
     params.require(:user).permit(:name, :email, :password, :firstname, :lastname, :nickname, :displayname,
       :phone, :family, :line, :pledge_class, :membership_status, :major, :graduation_year, :shirt_size, :car,
       :password, :password_confirmation)
-	end
+    end
 
-	def greensheet_section_params 
+    def greensheet_section_params 
     params.require(:greensheet_section).permit(:title, :start_time, :user_id, :hours, :chair, :event_type, :original_event_type)
-	end
+    end
 
-	def ensure_user
-		@user = User.find_by_a_username(params[:id])
-		@user ||= User.find(params[:id])
+    def ensure_user
+        @user = User.find_by_a_username(params[:id])
+        @user ||= User.find(params[:id])
     unless user_signed_in? && (current_user.id == @user.id || current_user.admin)
       redirect_to root_path, notice: "You do not have permission to see that page."
     end
